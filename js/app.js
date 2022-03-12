8de712ff503d447380f780c58dbdbc3ff3a3c162
JEBA <fawjiajeba@gmail.com>
2022-03-12 23:33:57 +0600

    final error

--- js/app.js
+++ js/app.js
@@ -142,6 +142,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById("liked").innerHTML = "";
   const likedPosts = getLikedPosts();
   likedPosts.forEach((post) => {
     const div = createPost(post);
@@ -150,11 +151,14 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+  document.getElementById("reported").innerHTML = "";
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
+    return;
   });
+
 };
 
 const loadPosts = async () => {
